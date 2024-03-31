c0796936dc106cfe3ac2e120ad02687d8a2a20d0
Viacheslav <vya4eslav.tka4enko@gmail.com>
2024-03-31 21:24:06 +0100

    context_manager

---
+++ contenxt_manager.py
@@ -0,0 +1,23 @@
+class ManagedFile:
+    def __init__(self,filename):
+        print('init')
+        self.filename = filename
+    
+    
+    def __enter__(self):
+        print('enter')
+        self.file = open(self.filename,'w')
+        return self.file
+    
+    def __exit__(self,exc_type,exc_value,exc_traceback):
+        if self.file:
+            self.file.close()
+        if exc_type is not None:
+            print('exception has been handle')
+        print('exit')
+        return True
+    
+with ManagedFile('notes.txt') as file:
+    print('do some stuff...')
+    file.write('some todo...')
+
